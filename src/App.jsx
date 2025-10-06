5a44394d21353a2e22d6dda09ef7f6de4942a82f
Rahin Haque <haquerahin743@gmail.com>
2025-10-06 22:20:46 +0600

    Added React Router

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <small className='text-6xl ' class="text-muted">Help Text</small>
+      
+    </div>
+  );
+};
+
+export default App;
